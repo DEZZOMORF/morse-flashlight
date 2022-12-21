081e71230bb48cac1fb14e4e597e34728651c96a
Pavlo Netrebchuk <p.netrubchuk@lampalampa.net>
2022-12-21 17:04:31 +0200

    Fixed strings

--- app/src/main/java/com/dezzomorf/morseflashlight/manager/InAppUpdateManager.kt
+++ app/src/main/java/com/dezzomorf/morseflashlight/manager/InAppUpdateManager.kt
@@ -2,6 +2,7 @@ package com.dezzomorf.morseflashlight.manager
 
 import android.app.Activity
 import android.widget.Toast
+import com.dezzomorf.morseflashlight.R
 import com.google.android.play.core.appupdate.AppUpdateInfo
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
@@ -11,7 +12,7 @@ import com.google.android.play.core.install.model.AppUpdateType
 import com.google.android.play.core.install.model.InstallStatus
 import com.google.android.play.core.install.model.UpdateAvailability
 
-class InAppUpdateManager(activity: Activity): InstallStateUpdatedListener {
+class InAppUpdateManager(private val activity: Activity): InstallStateUpdatedListener {
 
     companion object {
         private const val UPDATE_REQUEST_CODE = 500
@@ -39,8 +40,7 @@ class InAppUpdateManager(activity: Activity): InstallStateUpdatedListener {
         appUpdateManager.appUpdateInfo.addOnSuccessListener { info ->
             if (currentType == AppUpdateType.FLEXIBLE) {
                 if (info.installStatus() == InstallStatus.DOWNLOADED)
-                    updatingToast()
-                    appUpdateManager.completeUpdate()
+                    completeUpdate()
             } else if (currentType == AppUpdateType.IMMEDIATE) {
                 if (info.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
                     startUpdate(info, AppUpdateType.IMMEDIATE)
@@ -54,14 +54,14 @@ class InAppUpdateManager(activity: Activity): InstallStateUpdatedListener {
         appUpdateManager.unregisterListener(this)
     }
 
-    private fun updatingToast() {
-        Toast.makeText(parentActivity, "Updating", Toast.LENGTH_SHORT).show()
+    private fun completeUpdate() {
+        Toast.makeText(parentActivity, activity.getString(R.string.updating), Toast.LENGTH_LONG).show()
+        appUpdateManager.completeUpdate()
     }
 
     override fun onStateUpdate(state: InstallState) {
         if (state.installStatus() == InstallStatus.DOWNLOADED) {
-            updatingToast()
-            appUpdateManager.completeUpdate()
+            completeUpdate()
         }
     }
 }
